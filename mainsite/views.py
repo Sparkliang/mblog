3876f5068593cb3498322f1de50ff63ec6e6cb4d
Sparkliang <liang-x17@mails.tsinghua.edu.cn>
2018-07-18 21:24:03 +0800

    4th commit

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.template.loader import get_template
+#from django.shortcuts import render
+from django.shortcuts import redirect
 from django.http import HttpResponse
+from datetime import datetime
 from .models import Post
 
 
 # Create your views here.
 def homepage(request):
+	template = get_template('index.html')
+	posts = Post.objects.all()
+	now = datetime.now()
+	html = template.render(locals())
+	return HttpResponse(html)
+
+"""def homepage(request):
 	posts = Post.objects.all()
 	post_lists = list()
 	for count, post in enumerate(posts):
@@ -16,3 +26,15 @@ def homepage(request):
 
 
 	return HttpResponse(post_lists)
+"""
+
+def showpost(request, slug):
+	template = get_template('post.html')
+	try:
+		post = Post.objects.get(slug = slug)
+		if post != None:
+			html = template.render(locals())
+			return HttpResponse(html)
+	except:
+		return redirect('/')
+
--- mblog/urls.py
+++ mblog/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import include, url
 #from django.urls import path
-from mainsite.views import homepage
+from mainsite.views import homepage, showpost
 
 
 urlpatterns = [
  # path('admin/', admin.site.urls),
 url(r'^$', homepage),
+url(r'^post/(\w+)$', showpost),
 url(r'^admin/', admin.site.urls),
 ]
